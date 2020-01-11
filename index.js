aa1453db2826167d9214eed3dbc3d2d221353be6
redgl0w <joachimlf@pm.me>
2020-01-11 18:21:30 +0100

    Blacklisted MDR in fr channels

--- index.js
+++ index.js
@@ -55,6 +55,10 @@ client.on('message', msg => {
         });
         msg.channel.send(response);
     }
+
+    if(msg.content.includes("mdr" && msg.channel.name.includes("fr"))){
+        msg.reply("In English please!");
+    }
 });
 
 ON_DEATH(function(signal, err) {
