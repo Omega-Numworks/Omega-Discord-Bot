4cafc7665c01db3938ebc985328a0fcb67e379f8
Colin Thomas <contact@nwa2coco.fr>
2020-03-05 14:55:20 +0100

    Cleaned up code Added channel id in configuration

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
 const yaml = require("yaml");
-const request = require("request")
-const moment = require("moment")
+const request = require("request");
+const moment = require("moment");
 const ON_DEATH = require('death');
 
 
 // CONFIG
-const ConfigLocation = "config.yaml"
-const config = yaml.parse(fs.readFileSync(ConfigLocation, "utf8"))
-const Commands = config.Commands
+const ConfigLocation = "config.yaml";
+const config = yaml.parse(fs.readFileSync(ConfigLocation, "utf8"));
+const Commands = config.Commands;
 
 client.on('ready', () => {
   console.log(`Connected as ${client.user.tag}!`);
@@ -39,8 +39,8 @@ client.on('message', msg => {
         }
     }
 
-    IssueNumberPosition = msg.content.indexOf("#")
-    if (IssueNumberPosition > -1 && msg[IssueNumberPosition + 1] != "<"){
+    IssueNumberPosition = msg.content.indexOf("#");
+    if (IssueNumberPosition > -1 && msg[IssueNumberPosition + 1] !== "<"){
         if(msg.content.substring(IssueNumberPosition+1).includes(" ")){
             IssueId = msg.content.substring(IssueNumberPosition+1).split(" ")[0]
         } else {
@@ -53,71 +53,71 @@ client.on('message', msg => {
                 'User-Agent' : 'Omega-Discord-Bot'
             }
         }, (error, response, body) => {
-            body = JSON.parse(body)
+            body = JSON.parse(body);
             if(error)
-                msg.channel.send("ERROR : " + error.toString())
-            if(response.statusCode == 200){
+                msg.channel.send("ERROR : " + error.toString());
+            if(response.statusCode === 200){
                 let message = new Discord.RichEmbed()
                     .setURL(body.html_url)
                     .setTitle(body.title + " (#" + body.number + ")")
                     .setAuthor(body.user.login, body.user.avatar_url, body.user.html_url)
                     .setDescription(body.body)
-                    .setTimestamp(Date.parse(body.created_at))
-                let AdditionalInformations = ""
+                    .setTimestamp(Date.parse(body.created_at));
+                let AdditionalInformations = "";
                 if(body.state !== "open"){
-                    AdditionalInformations+=":x: Closed by "+ body.closed_by.login + " " + moment(body.closed_at).fromNow() +" (" + moment(body.closed_at).format("D, MMMM YYYY, HH:mm:ss") +" )\n"
+                    AdditionalInformations+=":x: Closed by "+ body.closed_by.login + " " + moment(body.closed_at).fromNow() +" (" + moment(body.closed_at).format("D, MMMM YYYY, HH:mm:ss") +" )\n";
                     message.setColor("a30000")
                 }else{
-                    AdditionalInformations += ":white_check_mark: Open\n"
+                    AdditionalInformations += ":white_check_mark: Open\n";
                     message.setColor("2b2b2b")
                 }
-                if(body.labels.length != 0){
-                    AdditionalInformations += ":label: Labels : "
+                if(body.labels.length !== 0){
+                    AdditionalInformations += ":label: Labels : ";
                     body.labels.forEach((item, index) => {
-                        if(index != 0){
+                        if(index !== 0){
                             AdditionalInformations+= ", "
                         }
                         AdditionalInformations+=item.name 
-                    })
+                    });
                     AdditionalInformations+="\n"
                 }
-                if(body.assignees.length != 0){
-                    AdditionalInformations+=":person_frowning: Assigned to "
+                if(body.assignees.length !== 0){
+                    AdditionalInformations+=":person_frowning: Assigned to ";
                     body.assignees.forEach((item, index) => {
-                        if(index != 0){
+                        if(index !== 0){
                             AdditionalInformations += ", "
                         }
                         AdditionalInformations+=item.login
-                    })
+                    });
                     AdditionalInformations+="\n"
                 }
                 if(body.locked){
                     AdditionalInformations+=":lock: locked\n"
                 }
-                if(body.pull_request != undefined){
+                if(body.pull_request !== undefined){
                     AdditionalInformations += ":arrows_clockwise: Pull request\n"
                 }
-                if(body.comments != 0){
+                if(body.comments !== 0){
                     AdditionalInformations+=":speech_balloon: Comments : " + body.comments + "\n"
                 }
-                if(IssueId.toLowerCase() == body.number + "c"){
+                if(IssueId.toLowerCase() === body.number + "c"){
                     request({
                         url : body.comments_url,
                         headers : {
                             'User-Agent' : 'Omega-Discord-Bot'
                         }
                     },(err, resp, bod) => {
-                        bod = JSON.parse(bod)
+                        bod = JSON.parse(bod);
                         bod.forEach((item) => {
                             message.addField("**Answer of** " + item.user.login + " **" + moment(item.created_at).fromNow() +" (" + moment(item.created_at).format("D, MMMM YYYY, HH:mm:ss") +" )**", item.body)
-                        })
+                        });
                         message.addField("Additional informations", AdditionalInformations)
-                            .setFooter(client.user.tag, client.user.avatarURL)
+                            .setFooter(client.user.tag, client.user.avatarURL);
                         msg.channel.send(message)
                     })
                 } else {
                     message.addField("Additional informations", AdditionalInformations)
-                        .setFooter(client.user.tag, client.user.avatarURL)
+                        .setFooter(client.user.tag, client.user.avatarURL);
                     msg.channel.send(message)
                 }
 
@@ -125,16 +125,16 @@ client.on('message', msg => {
         })
     }
 
-    if(msg.channel.id === "685114510259912724"){
+    if(msg.channel.id === config.Channel){
         setTimeout(() => {msg.delete(0)}, 2000);
     }
 
     if(!msg.content.startsWith(config.Prefix))
         return;
 
-    WithoutPrefix = msg.content.replace(config.Prefix, "")
-    Command = WithoutPrefix.split(" ")[0]
-    if(Command == Commands.help.input){
+    WithoutPrefix = msg.content.replace(config.Prefix, "");
+    Command = WithoutPrefix.split(" ")[0];
+    if(Command === Commands.help.input){
         let response = new Discord.RichEmbed()
             .setTitle("Here are the commands you can use with me")
             .setThumbnail(client.user.displayAvatarURL)
@@ -146,7 +146,7 @@ client.on('message', msg => {
             response.addField(config.Prefix + item.input, item.description)
         }
         msg.channel.send(response);
-    } else if(Command == Commands.repository.input){
+    } else if(Command === Commands.repository.input){
         let response = new Discord.RichEmbed()
             .setTitle("Here are the repositories of the omega project")
             .setTimestamp(new Date())
@@ -157,15 +157,15 @@ client.on('message', msg => {
             response.addField(item.name, item.desc + " (" + item.url + ")")
         }
         msg.channel.send(response);
-    } else if(Command == Commands.team.input){
+    } else if(Command === Commands.team.input){
         let response = new Discord.RichEmbed()
             .setTitle("Here are the people who develop the omega project")
             .setTimestamp(new Date())
             .setURL(config.URL)
             .setAuthor(client.user.tag, client.user.displayAvatarURL, config.URL);
-        let team = JSON.parse(fs.readFileSync(Commands.team.file))
+        let team = JSON.parse(fs.readFileSync(Commands.team.file));
         team.forEach(element => {
-            response.addField(element.name, "Github : " + element.Github + " Discord : " + client.users.find(user => user.id == element.DiscordId).tag)
+            response.addField(element.name, "Github : " + element.Github + " Discord : " + client.users.find(user => user.id === element.DiscordId).tag)
         });
         msg.channel.send(response);
     }
@@ -175,6 +175,6 @@ ON_DEATH(function(signal, err) {
     console.log("Destroying the bot.");
     client.destroy();
     process.exit();
-})
+});
 
 client.login(config.Token);
