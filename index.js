5a90149e295aa05af12e751c2b7cd3be5e4ee910
Charlotte THOMAS <contact@nwa2coco.fr>
2020-03-07 15:37:58 +0100

    [Feature] Weeb & NASA Commands (#13)
    * add timeout suppressing message
    
    * Cleaned up code
    Added channel id in configuration
    
    * pass from 2 to 7s before deletion
    
    * revert bot config
    
    * Adding algorithm to choose duration ( min : 100ms, max 7s, default: 7s)
    
    * remove debug
    
    * Update blank_config.yaml
    
    Co-Authored-By: redgl0w <43498612+RedGl0w@users.noreply.github.com>
    
    * move max/min time into configuration
    
    * Increased to 30s default
    
    * Added issue number for Epsilon
    Added Epsilon-Repository in config
    Added <#number> to not print issue
    
    * remove debug
    
    * Adding a message that warns if someone say a forbidden word like "crypté" beurk.
    
    * fix
    
    * added hug command
    
    * add credits
    
    * added cuddle & kiss
    
    * Added pat, waifu, feed and owo commands
    
    * add fact command
    escape character in owo command
    
    * add kemonomimi
    
    * added APOD command
    
    * Add choosing date
    
    * Quentin and the gang broke the bot
    This is my revenge
    (seriously: i added some test to avoid undefined queries)
    
    Co-authored-by: redgl0w <43498612+RedGl0w@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,15 +5,41 @@ const yaml = require("yaml");
 const request = require("request");
 const moment = require("moment");
 const ON_DEATH = require('death');
+const fetch = require("node-fetch");
 
 
 // CONFIG
 const ConfigLocation = "config.yaml";
 const config = yaml.parse(fs.readFileSync(ConfigLocation, "utf8"));
 const Commands = config.Commands;
+const nasa = config["NASA_API_KEY"];
+const forbiddenList = [
+    "crypter",
+    "crypté",
+    "cryptage",
+    "cryptation",
+    "encrypter"
+];
+
+let chiffrer = NaN;
+
 
 client.on('ready', () => {
     console.log(`Connected as ${client.user.tag}!`);
+    request(
+        {
+            url: "https://chiffrer.info",
+            headers: {
+                "User-Agent": "Omega-Discord-Bot"
+            }
+        },
+        (error, response, body) => {
+            chiffrer = new Discord.RichEmbed()
+                .setURL("https://chiffrer.info")
+                .setAuthor("La Langue Française", "https://chiffrer.info/wp-content/uploads/2016/07/ic_lock_outline_black_48dp_2x.png", "https://chiffrer.info")
+                .setTimestamp(new Date().getTime())
+                .setDescription("ON DIT CHIFFRER, ET PAS CRYPTER. :-)")
+        });
 });
 
 client.on('message', msg => {
@@ -38,7 +64,23 @@ client.on('message', msg => {
             return
         }
     }
-    let cloneMsg = msg.content
+
+
+    let cloneMsg = msg.content;
+
+    if (cloneMsg.toLowerCase() === "good bot") {
+        msg.reply("Good human!");
+    }
+    if (cloneMsg.toLowerCase() === "bad bot") {
+        msg.reply("Sorry :(");
+    }
+
+    for (let forbidden in forbiddenList) {
+        if (cloneMsg.toLowerCase().includes(forbiddenList[forbidden])) {
+            msg.channel.send(chiffrer);
+            break;
+        }
+    }
     IssueNumberPosition = msg.content.indexOf("#");
     if (IssueNumberPosition > -1 && msg[IssueNumberPosition + 1] !== "<") {
         if (msg.content.substring(IssueNumberPosition + 1).includes(" ")) {
@@ -151,15 +193,18 @@ client.on('message', msg => {
             durationComplete = config.DurationMax;
         else if (durationComplete <= config.DurationMin)
             durationComplete = config.DurationMin;
-        msg.delete(durationComplete);
+        setTimeout(() => {
+            msg.delete(0).catch(reason => console.log("The Message was already destroyed"))
+        }, durationComplete)
 
     }
 
     if (!msg.content.startsWith(config.Prefix))
         return;
 
-    WithoutPrefix = msg.content.replace(config.Prefix, "");
-    Command = WithoutPrefix.split(" ")[0];
+    let WithoutPrefix = msg.content.replace(config.Prefix, "");
+    let Command = WithoutPrefix.split(" ")[0];
+    let commandsargs = WithoutPrefix.split(" ").shift().toLowerCase();
     if (Command === Commands.help.input) {
         let response = new Discord.RichEmbed()
             .setTitle("Here are the commands you can use with me")
@@ -194,8 +239,133 @@ client.on('message', msg => {
             response.addField(element.name, "Github : " + element.Github + " Discord : " + client.users.find(user => user.id === element.DiscordId).tag)
         });
         msg.channel.send(response);
+    } else if (Command === Commands.hug.input) {
+        if (!msg.mentions.users.size) {
+            msg.reply('Are you alone :( ?')
+            return;
+        }
+        sendHug(msg, "hug", "hugged");
+    } else if (Command === Commands.kiss.input) {
+        if (!msg.mentions.users.size) {
+            msg.reply('Are you alone :( ?')
+            return;
+        }
+        sendHug(msg, "kiss", "kissed");
+    } else if (Command === Commands.cuddle.input) {
+        if (!msg.mentions.users.size) {
+            msg.reply('Are you alone :( ?')
+            return;
+        }
+        sendHug(msg, "cuddle", "cuddled");
+    } else if (Command === Commands.pat.input) {
+        if (!msg.mentions.users.size) {
+            msg.reply('Are you alone :( ?')
+            return;
+        }
+        sendHug(msg, "pat", "head-patted");
+    } else if (Command === Commands.waifu.input) {
+        sendImage(msg, "waifu", "waifu");
+    } else if (Command === Commands.feed.input) {
+        if (!msg.mentions.users.size) {
+            msg.reply('Are you alone :( ?')
+            return;
+        }
+        sendHug(msg, "feed", "fed");
+    } else if (Command === Commands.owo.input) {
+        let message = WithoutPrefix.substr(3, WithoutPrefix.length);
+        if (message.length === 0) {
+            msg.reply("Send a text :) !")
+            return;
+        }
+        owoify(msg, message);
+    } else if (Command === Commands.fact.input) {
+        fact(msg);
+    } else if (Command === Commands.kemonomimi.input) {
+        sendImage(msg, "kemonomimi", "picture");
+    } else if (Command === Commands.apod.input) {
+        let message = WithoutPrefix.substr(4, WithoutPrefix.length);
+        if (!moment(message, "YYYY-MM-DD").isValid()) {
+            message = "";
+        }
+        let s = moment(message, "YYYY-MM-DD");
+        if (s.isAfter(moment.now()))
+            message = "";
+        if (s.isBefore(moment("1995-06-17")))
+            message = "";
+        if (message.length > 0) {
+            apod(msg, message, false)
+            return;
+        }
+        apod(msg, message, true);
     }
 });
+
+async function sendHug(msg, action, verb) {
+    try {
+        const user = msg.mentions.users.first();
+        const data = await (await fetch('https://nekos.life/api/v2/img/' + action)).json();
+        if ((!(data || data.url)))
+            return msg.channel.send("an error occured");
+        let answer = new Discord.RichEmbed()
+            .setTitle("@" + user.username + "" + " is " + verb + " by @" + msg.author.username + "")
+            .setImage(data.url)
+            .addField("Provided by : ", "nekos.life")
+        msg.channel.send(answer)
+    } catch (e) {
+        console.log(e)
+        return msg.channel.send("an error occured")
+    }
+}
+
+async function sendImage(msg, action, text) {
+    try {
+        const data = await (await fetch('https://nekos.life/api/v2/img/' + action)).json();
+        if ((!(data || data.url)))
+            return msg.channel.send("an error occured");
+        let answer = new Discord.RichEmbed()
+            .setTitle("Here is your " + text)
+            .setImage(data.url)
+            .addField("Provided by : ", "nekos.life")
+        msg.channel.send(answer)
+    } catch (e) {
+        console.log(e)
+        return msg.channel.send("an error occured")
+    }
+}
+
+async function owoify(msg, text) {
+    const data = await (await fetch('https://nekos.life/api/v2/owoify?text=' + escape(text))).json();
+    if ((!(data || data.owo)))
+        return msg.channel.send("an error occured");
+    msg.reply(data.owo)
+}
+
+async function fact(msg) {
+    const data = await (await fetch('https://nekos.life/api/v2/fact')).json();
+    if ((!(data || data.fact))) {
+        return msg.channel.send("an error occured");
+    }
+    msg.reply("Fun fact : " + data.fact)
+}
+
+async function apod(msg, date, defaul) {
+
+    let link = 'https://api.nasa.gov/planetary/apod?api_key=' + nasa;
+    if (!defaul) {
+        date = date.trim().replace(" ", "-").replace(" ", "-").split(" ")[0];
+        link = link + "&date=" + date;
+    }
+    const data = await (await fetch(link)).json();
+    if ((!(data || data.url))) {
+        return msg.channel.send("an error occured");
+    }
+    let answer = new Discord.RichEmbed()
+        .setTitle("NASA Astronomy Picture of the Day")
+        .setAuthor(data.copyright)
+        .setImage(data.url)
+        .setDescription(data.explanation);
+    msg.channel.send(answer);
+}
 
 ON_DEATH(function (signal, err) {
     console.log("Destroying the bot.");
